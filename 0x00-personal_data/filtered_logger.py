8ab1692e76cfc077934bd9b94b5eae27dee7e6d6
TobyMike-max <osinubi.michael@gmail.com>
2023-02-02 14:57:29 +0100

    [Create]: Files for task 0 and README

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Regex-ing"""
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Return the log message obfuscated"""
+    for field in fields:
+        message = re.sub("{}=(.*?){}".format(field, separator),"{}={}{}"
+                         .format(field, redaction, separator), message)
+    return message
