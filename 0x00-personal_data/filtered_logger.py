74cb76aa3d86c572249645a1c1f4f8dc0f5f5544
TobyMike-max <osinubi.michael@gmail.com>
2023-02-02 15:23:23 +0100

    [Modified]: Corrected spacing for parameters

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Regex-ing"""
+
 from typing import List
 
 
@@ -7,6 +8,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Return the log message obfuscated"""
     for field in fields:
-        message = re.sub("{}=(.*?){}".format(field, separator),"{}={}{}"
+        message = re.sub("{}=(.*?){}".format(field, separator), "{}={}{}"
                          .format(field, redaction, separator), message)
     return message
